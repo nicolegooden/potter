1dabd5b23f174c5fb26793b67214c0644d33b277
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-06 09:40:30 -0700

    Move all App files to App directory

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+  
+  render() {
+    return (
+      <main>
+        <h1>hi</h1>
+      </main>
+    )
+  }
+}
+
+export default App;
