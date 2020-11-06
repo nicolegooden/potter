2a86b336f2b199943bcbc55fb3f5cee708a36c93
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-06 11:04:26 -0700

    Add section that contains sorting hat as child

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './HomePage.css';
+import sortingHat from '../sorting-hat-image.png'
 
 const HomePage = (props) => {
   return (
-    <main className='homepage-container'>
-
-    </main>
+    <section className='homepage-container'>
+      <article className='sorting-hat-container'>
+        <img className='sorting-hat-image' src={sortingHat} alt='sorting hat'/>
+      </article>
+    </section>
   )
 }
 
