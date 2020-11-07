d4613e7a9a8bd767e32cbaa70d2e767d0fdf99ba
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-06 20:21:01 -0700

    Create getCharacters() to fetch all characters from Hogwarts

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,18 @@
+const apiKey = '?key=$2a$10$2D2n5mxwx4zUWsnRGmwA.uG0fMQF5JDCqPxn7gKc.Iu2STV/3K3zW';
+
 export const getSorted = async () => {
   const response = await fetch('https://www.potterapi.com/v1/sortingHat')
+  try {
+    if (response.ok) {
+      return response.json();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const getCharacters = async () => {
+  const response = await fetch(`https://www.potterapi.com/v1/characters/${apiKey}`)
   try {
     if (response.ok) {
       return response.json();
