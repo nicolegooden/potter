fa94e16de38a0218007a8f34e5774e9e86d433fe
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-07 09:30:10 -0700

    Add conditional rendering for select button based on whether a character has been finalized

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './CharacterCard.css';
 
 const CharacterCard = (props) => {
+  let button;
+  if (!props.myCharacter) {
+    button = <button onClick={() => props.setTempCharacterDetails(props.name, props.id)} className='select-button'>select</button>
+  }
   return (
     <article className='character-article'>
       <h1 className='character-name'>{props.name}</h1>
       <h3 className='character-blood-status'>Blood Status: {props.bloodStatus}</h3>
       <h3 className='character-species'>Species: {props.species}</h3>
       <div className='button-wrapper'>
-        <button onClick={() => props.setCharacter(props.id)} className='select-button'>select</button>
+        {button}      
       </div>
     </article>
   )
