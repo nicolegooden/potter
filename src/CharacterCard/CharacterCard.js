0bb4bf3374a2506299df2604ba95f76e9b4e3b84
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-06 21:10:37 -0700

    Add button and more descriptive detail labels

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './CharacterCard.css';
 
 const CharacterCard = (props) => {
   return (
-    <article className='character-section'>
+    <article className='character-article'>
       <h1 className='character-name'>{props.name}</h1>
-      <h2 className='character-blood-status'>Blood Status: {props.bloodStatus}</h2>
-      <h2 className='character-species'>Species: {props.species}</h2>
+      <h3 className='character-blood-status'>Blood Status: {props.bloodStatus}</h3>
+      <h3 className='character-species'>Species: {props.species}</h3>
+      <div className='button-wrapper'>
+        <button className='choose-button'>choose</button>
+      </div>
     </article>
   )
 }
