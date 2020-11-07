fce5964fbd970bb39d7bb19a1951526f9fa83624
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-07 16:13:43 -0700

    Add test to ensure select button is not display after character is finalized

--- src/CharacterCard/CharacterCard.test.js
+++ src/CharacterCard/CharacterCard.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import userEvent from '@testing-library/user-event';
 import CharacterCard from './CharacterCard.js';
 import { MemoryRouter } from 'react-router-dom';
 import { getCharacters } from '../apiCalls.js';
@@ -91,6 +90,46 @@ describe('Character Card', () => {
     const name = await waitFor(() => screen.getByText('Hermione Granger'))
     expect(name).toBeInTheDocument();
     expect(screen.getByRole('button', {name: 'select'})).toBeInTheDocument()
+    expect(screen.getByText('Blood Status: muggle-born')).toBeInTheDocument();
+    expect(screen.getByText('Species: human')).toBeInTheDocument();
+  })
+
+  it('should no longer render the select button if myCharacter is stored', async () => {
+    let mockID = 'ghi789';
+    let mockName = 'Hermione Granger';
+    let mockBloodStatus = 'muggle-born';
+    let mockSpecies = 'human';
+    let mockSetTemp = jest.fn();
+    let mockMyCharacter = {
+      _id: 'ghi789',
+      name: 'Hermione Granger',
+      role: 'student',
+      house: 'Gryffindor',
+      school: 'Hogwarts',
+      ministryOfMagic: false,
+      orderOfThePhoenix: true, 
+      dumbledoresArmy: true,
+      deathEater: false,
+      bloodStatus: 'muggle-born',
+      species: 'human'
+    }
+
+    render(
+      <MemoryRouter>
+        <CharacterCard 
+          id={mockID}
+          name={mockName}
+          bloodStatus={mockBloodStatus}
+          species={mockSpecies}
+          setTempCharacterDetails={mockSetTemp}
+          myCharacter={mockMyCharacter}
+        /> 
+      </MemoryRouter>
+    )
+
+    const name = await waitFor(() => screen.getByText('Hermione Granger'))
+    expect(name).toBeInTheDocument();
+    expect(screen.queryByText('select')).toBeNull()
     expect(screen.getByText('Blood Status: muggle-born')).toBeInTheDocument();
     expect(screen.getByText('Species: human')).toBeInTheDocument();
   })
