429e329b675d7602ebbaabe9b1b72d6c4d18198f
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-06 17:10:15 -0700

    Create houseButton variable to be rendered based on house existence

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -4,17 +4,20 @@ import sortingHat from '../sorting-hat-image.png'
 
 const HomePage = (props) => {
   let message;
+  let houseButton;
   if (!props.house) {
     message = <h1 className='welcome'>Welcome, 1st Year!</h1>
+    houseButton = <button onClick={props.setHouse} className='house-button'>find my house</button>
   } else {
     message = <h1 className='welcome'>You are... {props.house}!</h1>
+    houseButton = <button className='house-button'>learn about {props.house}</button>
   }
 
   return (
     <section className='homepage-container'>
       {message}
       <img className='sorting-hat-image' src={sortingHat} alt='sorting hat'/>
-      {props.changeHomePageButton()}
+      {houseButton}
     </section>
   )
 }
