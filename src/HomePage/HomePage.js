332e7eda449768b8be39f42def94d5f06e6cc122
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-06 18:00:40 -0700

    Create characterButton to be rendered only when character has chosen a house

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './HomePage.css';
 import sortingHat from '../sorting-hat-image.png'
+import { Link } from 'react-router-dom';
 
 const HomePage = (props) => {
   let message;
   let houseButton;
+  let characterButton;
+  
   if (!props.house) {
-    message = <h1 className='welcome'>Welcome, 1st Year!</h1>
+    message = <h1 className='message'>Welcome, 1st Year!</h1>
     houseButton = <button onClick={props.setHouse} className='house-button'>find my house</button>
   } else {
-    message = <h1 className='welcome'>You are... {props.house}!</h1>
+    message = <h1 className='message'>You are... {props.house}!</h1>
     houseButton = <button className='house-button'>learn about {props.house}</button>
+    characterButton = (
+      <Link to='/characters' className='character-container'>
+        <button className='character-button'>choose character</button>
+      </Link>
+    )
   }
 
   return (
@@ -18,6 +26,7 @@ const HomePage = (props) => {
       {message}
       <img className='sorting-hat-image' src={sortingHat} alt='sorting hat'/>
       {houseButton}
+      {characterButton}
     </section>
   )
 }
