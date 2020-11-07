b178bbe2f5dcf34c0ebf7a61540c77993890f82b
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-07 10:33:35 -0700

    Add NavLink components to wrap each text element in the header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Header.css';
 import HP from '../hp-logo.png'
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <section className='header-container'>
       <img src={HP} className='hp-logo' alt='Harry Potter logo'></img>
       <article className='nav-container'>
-        <h1 className='header-link'>home</h1>
-        <h1 className='header-link'>characters</h1>
-        <h1 className='header-link'>spells</h1>
+        <NavLink to='/' className='nav-link'><h1 className='header-link'>home</h1></NavLink>
+        <NavLink to='/characters' className='nav-link'><h1 className='header-link'>characters</h1></NavLink>
+        <NavLink to='/spells' className='nav-link'><h1 className='header-link'>spells</h1></NavLink>
       </article>
     </section>
   )
