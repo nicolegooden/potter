8a4ede4169c2478b8f3cfc10f46fb3070f654ab8
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-07 16:05:02 -0700

    Add test to ensure elements render as expected

---
+++ src/CharacterCard/CharacterCard.test.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import CharacterCard from './CharacterCard.js';
+import { MemoryRouter } from 'react-router-dom';
+import { getCharacters } from '../apiCalls.js';
+jest.mock('../apiCalls.js');
+
+describe('Character Card', () => {
+  beforeEach(() => {
+    getCharacters.mockResolvedValueOnce([
+      {
+        _id: 'abc123',
+        name: 'Harry Potter',
+        role: 'student',
+        house: 'Gryffindor',
+        school: 'Hogwarts',
+        ministryOfMagic: false,
+        orderOfThePhoenix: true, 
+        dumbledoresArmy: true,
+        boggart: 'Dementor',
+        deathEater: false,
+        bloodStatus: 'half-blood',
+        species: 'human'
+      },
+      {
+        _id: 'def456',
+        name: 'Draco Malfoy',
+        role: 'student',
+        house: 'Slytherin',
+        school: 'Hogwarts',
+        ministryOfMagic: false,
+        orderOfThePhoenix: false, 
+        dumbledoresArmy: false,
+        deathEater: true,
+        bloodStatus: 'pure-blood',
+        species: 'human'
+      },
+      {
+        _id: 'ghi789',
+        name: 'Hermione Granger',
+        role: 'student',
+        house: 'Gryffindor',
+        school: 'Hogwarts',
+        ministryOfMagic: false,
+        orderOfThePhoenix: true, 
+        dumbledoresArmy: true,
+        deathEater: false,
+        bloodStatus: 'muggle-born',
+        species: 'human'
+      },
+      {
+        _id: 'jkl101',
+        name: 'Charity Burbage',
+        role: 'Professor, Muggle Studies',
+        school: 'Hogwarts',
+        ministryOfMagic: true,
+        orderOfThePhoenix: false, 
+        dumbledoresArmy: false,
+        deathEater: false,
+        bloodStatus: 'unknown',
+        species: 'human'
+      }
+    ])
+  })
+
+
+  it('should render all expected elements', async () => {
+
+    let mockID = 'ghi789';
+    let mockName = 'Hermione Granger';
+    let mockBloodStatus = 'muggle-born';
+    let mockSpecies = 'human';
+    let mockSetTemp = jest.fn();
+    let mockMyCharacter = null
+
+    render(
+      <MemoryRouter>
+        <CharacterCard 
+          id={mockID}
+          name={mockName}
+          bloodStatus={mockBloodStatus}
+          species={mockSpecies}
+          setTempCharacterDetails={mockSetTemp}
+          myCharacter={mockMyCharacter}
+        /> 
+      </MemoryRouter>
+    )
+
+    const name = await waitFor(() => screen.getByText('Hermione Granger'))
+    expect(name).toBeInTheDocument();
+    expect(screen.getByRole('button', {name: 'select'})).toBeInTheDocument()
+    expect(screen.getByText('Blood Status: muggle-born')).toBeInTheDocument();
+    expect(screen.getByText('Species: human')).toBeInTheDocument();
+  })
+})
