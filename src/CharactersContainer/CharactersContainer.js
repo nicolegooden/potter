c110502143d3bb7356a4209951c503f902928b6f
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-06 19:48:00 -0700

    Add and style background image for section

---
+++ src/CharactersContainer/CharactersContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CharactersContainer.css';
+
+const CharactersContainer = () => {
+  return (
+    <section className='all-characters-section'>
+      
+    </section>
+  )
+}
+
+export default CharactersContainer;
