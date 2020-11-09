1e475545f9e3915b1e8eb297666b26e6861fe78d
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-09 16:22:46 -0700

    Update test id for add and practice buttons based on spell's id

--- src/SpellCard/SpellCard.js
+++ src/SpellCard/SpellCard.js
@@ -11,7 +11,7 @@ const SpellCard = (props) => {
   })
 
   if (props.myCharacter && mySpellIndex === -1) {
-    button = <button data-testid={`add button for ${props.spell.key}`} onClick={() => props.addSpell(props.spell)} className='add-spell-button'>add</button>
+    button = <button data-testid={`add button for ${props.spell._id}`} onClick={() => props.addSpell(props.spell)} className='add-spell-button'>add</button>
   } else if (props.myCharacter) {
     let mastery;
     props.spell.points >= 15 ? mastery = 'Congrats! Fully mastered!' : mastery = 'Not yet mastered';
@@ -19,7 +19,7 @@ const SpellCard = (props) => {
       <article className='saved-info'>
         <h1 className='progress'>Points: {props.mySpells[mySpellIndex].points}</h1>
         <h1 className='mastery'>{mastery}</h1>
-        <button data-testid={`practice button for ${props.spell.key}`} onClick={() => props.declarePracticeSpell(props.spell)} className='practice-spell-button'>practice</button>
+        <button data-testid={`practice button for ${props.spell._id}`} onClick={() => props.declarePracticeSpell(props.spell)} className='practice-spell-button'>practice</button>
       </article>
     )
   }
