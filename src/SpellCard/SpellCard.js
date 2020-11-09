f434e767e3a9f5dc32b39e6d85818887c977da8e
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-08 22:05:21 -0700

    Add savedInfo to display points and mastery for a specific spell

--- src/SpellCard/SpellCard.js
+++ src/SpellCard/SpellCard.js
@@ -3,10 +3,17 @@ import './SpellCard.css';
 
 const SpellCard = (props) => {
   let button;
+  let savedInfo;
   if (props.myCharacter && !props.mySpells.includes(props.spell)) {
       button = <button onClick={() => props.addSpell(props.spell)} className='add-spell-button'>add</button>
   } else if (props.myCharacter && props.mySpells.includes(props.spell)) {
-    button = <button onClick={() => props.practiceSpell(props.spell)} className='practice-spell-button'>practice</button>
+    button = <button onClick={() => props.declarePracticeSpell(props.spell)} className='practice-spell-button'>practice</button>
+    savedInfo = (
+      <article className='saved-info'>
+        <h1 className='progress'>Points:</h1>
+        <h1 className='mastery'>Mastery:</h1>
+      </article>
+    )
   }
 
   return (
@@ -14,6 +21,7 @@ const SpellCard = (props) => {
       <h1 className='spell-detail spell-title'>{props.spell.spell}</h1>
       <h1 className='spell-detail'>{props.spell.type}</h1>
       <h1 className='spell-detail'>{props.spell.effect}</h1>
+      {savedInfo}
       {button}
     </article>
   )
