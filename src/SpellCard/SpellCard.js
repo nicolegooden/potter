b97a69f1694e6eaf87f6159a431f7df06c544062
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-08 22:32:23 -0700

    Change SpellCard to class component to track its own points and mastery

--- src/SpellCard/SpellCard.js
+++ src/SpellCard/SpellCard.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SpellCard.css';
 
-const SpellCard = (props) => {
-  let button;
-  let savedInfo;
-  if (props.myCharacter && !props.mySpells.includes(props.spell)) {
-      button = <button onClick={() => props.addSpell(props.spell)} className='add-spell-button'>add</button>
-  } else if (props.myCharacter && props.mySpells.includes(props.spell)) {
-    button = <button onClick={() => props.declarePracticeSpell(props.spell)} className='practice-spell-button'>practice</button>
-    savedInfo = (
+class SpellCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      points: 0,
+      mastery: false
+    }
+  }
+
+  determineDetails = () => {
+  if (this.props.myCharacter && !this.props.mySpells.includes(this.props.spell)) {
+    return <button onClick={() => this.props.addSpell(this.props.spell)} className='add-spell-button'>add</button>
+  } else if (this.props.myCharacter && this.props.mySpells.includes(this.props.spell)) {
+    return (
       <article className='saved-info'>
         <h1 className='progress'>Points:</h1>
         <h1 className='mastery'>Mastery:</h1>
+        <button onClick={() => this.props.declarePracticeSpell(this.props.spell)} className='practice-spell-button'>practice</button>
       </article>
     )
   }
+}
 
+  render() {
   return (
     <article className='spell-card'>
-      <h1 className='spell-detail spell-title'>{props.spell.spell}</h1>
-      <h1 className='spell-detail'>{props.spell.type}</h1>
-      <h1 className='spell-detail'>{props.spell.effect}</h1>
-      {savedInfo}
-      {button}
+      <h1 className='spell-detail spell-title'>{this.props.spell.spell}</h1>
+      <h1 className='spell-detail'>{this.props.spell.type}</h1>
+      <h1 className='spell-detail'>{this.props.spell.effect}</h1>
+      {this.determineDetails()}
     </article>
   )
+  }
 }
 
 export default SpellCard;
