ab1b7acc91bedd33eb9b6fed273ed487930ead1f
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-08 20:42:32 -0700

    Add condition to determine if practice or add button should show based on inclusion in mySpells

--- src/SpellCard/SpellCard.js
+++ src/SpellCard/SpellCard.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './SpellCard.css';
 
 const SpellCard = (props) => {
   let button;
-  if (props.myCharacter) {
+  if (props.myCharacter && !props.mySpells.includes(props.spell)) {
       button = <button onClick={() => props.addSpell(props.spell)} className='add-spell-button'>add</button>
-    }
+  } else if (props.myCharacter && props.mySpells.includes(props.spell)) {
+    button = <button onClick={() => props.practiceSpell(props.spell)} className='practice-spell-button'>practice</button>
+  }
+
   return (
     <article className='spell-card'>
       <h1 className='spell-detail spell-title'>{props.spell.spell}</h1>
