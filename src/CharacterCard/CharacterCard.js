872aecd0945ba273ccde76fafc2a9b03b361c449
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-23 15:53:17 -0700

    Add conditional rendering for select button and additional character details

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const CharacterCard = (props) => {
   let button;
-  if (!props.myCharacter) {
+  if (!props.myCharacter && props.house) {
     button = 
       <button 
         data-testid={`select button for ${props.name}`}  
@@ -18,6 +18,8 @@ const CharacterCard = (props) => {
       <h1 className='character-name'>{props.name}</h1>
       <h3 className='character-blood-status'>Blood Status: {props.bloodStatus}</h3>
       <h3 className='character-species'>Species: {props.species}</h3>
+      {!props.house && <h3 className='character-role'>Role: {props.role}</h3>}
+      {!props.house && <h3 className='character-house'>House: {props.myHouse}</h3>}
       <div className='button-wrapper'>
         {button}      
       </div>
@@ -28,7 +30,7 @@ const CharacterCard = (props) => {
 export default CharacterCard;
 
 CharacterCard.propTypes = {
-  id: PropTypes.string,
+  id: PropTypes.number,
   name: PropTypes.string.isRequired,
   bloodStatus: PropTypes.string.isRequired,
   species: PropTypes.string.isRequired,
