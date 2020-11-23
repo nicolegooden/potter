3b468d4958dc4419f00e8ed38a58414d7325a836
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-11-23 14:37:33 -0700

    Add onClick function to populate character cards from Header link

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './Header.css';
 import HP from '../hp-logo.png'
 import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <section className='header-container'>
       <img src={HP} className='hp-logo' alt='Harry Potter logo'></img>
       <article className='nav-container'>
         <NavLink to='/' className='nav-link'><h1 className='header-link'>home</h1></NavLink>
-        <NavLink to='/characters' className='nav-link'><h1 className='header-link'>characters</h1></NavLink>
+        <NavLink to='/characters' className='nav-link'><h1 onClick={props.getStudentsByHouse} className='header-link'>characters</h1></NavLink>
         <NavLink to='/spells' className='nav-link'><h1 className='header-link'>spells</h1></NavLink>
       </article>
     </section>
@@ -17,3 +18,7 @@ const Header = () => {
 }
 
 export default Header;
+
+Header.propTypes = {
+  getStudentsByHouse: PropTypes.func
+}
